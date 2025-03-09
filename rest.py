540329a7b22310ad2733b953b0892361a5f5436a
Kevin Stanton <kevin@sproutsocial.com>
2025-03-09 15:40:29 -0500

    liveness, readiness, startup probes

--- rest.py
+++ rest.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.route("/health")
+def healthcheck():
+    return ""
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
